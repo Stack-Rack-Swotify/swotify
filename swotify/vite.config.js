9c3aba60a90df4fc98ae18933f632f654cb2a235
Vyas S <vyas.sk17@gmail.com>
2025-12-12 11:56:25 +0530

    chore: Prepare for Netlify deployment (remove GH Actions, add netlify.toml)

--- swotify/vite.config.js
+++ swotify/vite.config.js
@@ -5,7 +5,6 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/swotify/',
   resolve: {
     alias: {
       'react': path.resolve(__dirname, 'node_modules/react'),
